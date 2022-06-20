9ae247545626cc6dda458064371413cccc01cb19
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-06-20 20:23:16 +0430

    remove data from minibase.js

--- src/lib/minibase.js
+++ src/lib/minibase.js
@@ -1,6 +1,6 @@
 import Minibase from "./minibase-sdk.js";
 import { getAttributes, getBC, getTags } from "./offline.js";
-import data from './offline-data.json'
+//import data from './offline-data.json'
 
 
 let appName = import.meta.env.VITE_MB_APPNAME
@@ -8,18 +8,18 @@ let apiKey = import.meta.env.VITE_MB_APIKEY
 
 let options = {
     mode: 'online',
-    data
+ //   data
 }
 const minibase = new Minibase(appName, apiKey, options)
 
-console.log(data.map(getTags).flat(2))
+//console.log(data.map(getTags).flat(2))
 
-if(options.mode !== 'online') {
+//if(options.mode !== 'online') {
 
-    await minibase.insert('tags', data.map(getTags).flat(2));
-    await minibase.insert('attributes', data.map(getAttributes).flat(2));
-    await minibase.insert('bc', data.map(getBC).flat(2));   
-}
+ //   await minibase.insert('tags', data.map(getTags).flat(2));
+ //   await minibase.insert('attributes', data.map(getAttributes).flat(2));
+ //   await minibase.insert('bc', data.map(getBC).flat(2));   
+//}
 
 // console.log((await minibase.get('tags')).length)
 
